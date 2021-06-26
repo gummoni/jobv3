58007ff4cc26ea6275dfdecdfc11f115595fb819
gummo <shaty3@hotmail.com>
2021-06-26 13:56:00 +0900

    循環リスト同士の結合に対応

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-
+/// <summary>
+/// ジョブ（処理単位）
+/// </summary>
 class job {
 
 private:
@@ -29,7 +31,6 @@ public:
 		for (job* last = this; ; last = last->action_next) {
 			if (NULL == last->action_next) {
 				last->action_next = self;
-				self->action_next = this;
 				return;
 			}
 		}
@@ -45,17 +46,23 @@ public:
 	}
 
 	/// <summary>
-	/// 並列処理追加
+	/// 循環リストの最後取得
+	/// </summary>
+	/// <param name="self"></param>
+	/// <returns></returns>
+	static job* fork_last(job* self) {
+		for (job* last = self; ; last = last->fork_next)
+			if (self == last->fork_next)
+				return last;
+	}
+
+	/// <summary>
+	/// 並列処理追加(２つの循環リストを繋げ合わせる)
 	/// </summary>
 	/// <param name="self"></param>
 	void fork(job* self) {
-		for (job* last = this; ; last = last->fork_next) {
-			if (this == last->fork_next) {
-				last->fork_next = self;
-				self->fork_next = this;
-				return;
-			}
-		}
+		fork_last(this)->fork_next = self;
+		fork_last(self)->fork_next = this;
 	}
 
 	/// <summary>
@@ -145,6 +152,8 @@ int main(void) {
 	auto j = new job1(-10, "A");
 	auto l = new job1(0, "C");
 	job* k = new job1(0, "B");
+	auto m = new job1(0, "M");
+	k->fork(m);
 	j->fork(k);
 	j->action(l);
 
