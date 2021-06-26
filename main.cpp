ef5e3c82f1bcfd729bcf45f1d9e0d497e13fe80e
gummoni <shaty3@hotmail.com>
2021-06-26 21:20:40 +0900

    C言語化

--- main.cpp
+++ main.cpp
@@ -1,146 +1,121 @@
 #include <stdio.h>
+#include "job.hpp"
 
-/// <summary>
-/// ジョブ（処理単位）
-/// </summary>
-class job {
+typedef void (*execute)(void*);
 
-private:
-	job* action_next = NULL;				//単方向リスト
-	job* fork_next = this;					//循環リスト
-	bool is_finished = false;				//完了状態
+typedef struct token {
+	struct token* action_next;
+	struct token* fork_next;
+	execute execute;
+	int errno;
+} token;
 
-	virtual void func() = 0;
 
-protected:
+#define suspend(self) { ((token*)self)->errno = JOB_STATE_BUSY; }
 
-	/// <summary>
-	/// 現在のジョブを休止
-	/// </summary>
-	void suspend() {
-		is_finished = false;
+token* fork_last(token* self) {
+	token* last = self;
+	while (self != last->fork_next)
+		last = last->fork_next;
+	return last;
+}
+
+void fork(token* self, token* item) {
+	fork_last(self)->fork_next = item;
+	fork_last(item)->fork_next = self;
+}
+
+void action(token* self, token* item) {
+	while (NULL != self->action_next)
+		self = self->action_next;
+	self->action_next = item;
+}
+
+token* dispatch(token* self) {
+	if (self->errno < JOB_STATE_FINISH) {
+		self->errno = JOB_STATE_FINISH;
+		self->execute(self);
+		if (self->errno == JOB_STATE_FINISH)
+		if (NULL != self->action_next)
+			fork(self, self->action_next);
 	}
 
-public:
+	for (; JOB_STATE_FINISH <= self->fork_next->errno; self->fork_next = self->fork_next->fork_next)
+		if (self == self->fork_next)
+			return NULL;
 
-	/// <summary>
-	/// 直列処理追加
-	/// </summary>
-	/// <param name="self"></param>
-	void action(job* self) {
-		for (job* last = this; ; last = last->action_next) {
-			if (NULL == last->action_next) {
-				last->action_next = self;
-				return;
-			}
-		}
-	}
+	return self->fork_next;
+}
 
-	/// <summary>
-	/// 直列処理追加
-	/// </summary>
-	/// <param name="jobs"></param>
-	void action(job** jobs) {
-		while (NULL != *jobs)
-			action(*(jobs++));
-	}
+void wait(token* self, token* cur) {
+	while (JOB_STATE_FINISH <= self->errno)
+		cur = dispatch(cur);
+}
 
-	/// <summary>
-	/// 循環リストの最後取得
-	/// </summary>
-	/// <param name="self"></param>
-	/// <returns></returns>
-	static job* fork_last(job* self) {
-		for (job* last = self; ; last = last->fork_next)
-			if (self == last->fork_next)
-				return last;
-	}
-
-	/// <summary>
-	/// 並列処理追加(２つの循環リストを繋げ合わせる)
-	/// </summary>
-	/// <param name="self"></param>
-	void fork(job* self) {
-		fork_last(this)->fork_next = self;
-		fork_last(self)->fork_next = this;
-	}
-
-	/// <summary>
-	/// 並列処理追加
-	/// </summary>
-	/// <param name="jobs"></param>
-	void fork(job** jobs) {
-		while (NULL != *jobs)
-			fork(*(jobs++));
-	}
-
-	/// <summary>
-	/// 完了待ち
-	/// </summary>
-	void wait() {
-		wait(this);
-	}
-
-	/// <summary>
-	/// 完了待ち
-	/// </summary>
-	/// <param name="cur"></param>
-	void wait(job* cur) {
-		while (!this->is_finished)
-			cur = cur->dispatch();
-	}
-
-	/// <summary>
-	/// 実行
-	/// </summary>
-	/// <param name="self"></param>
-	job* dispatch() {
-		//処理実行
-		is_finished = true;
-		func();
-
-		//完了したら直列シーケンスを並列シーケンスに登録
-		if (is_finished)
-			if (NULL != action_next)
-				fork(action_next);
-
-		//次に実行する並列シーケンスを返す
-		for (; fork_next->is_finished; fork_next = fork_next->fork_next)
-			if (this == fork_next)
-				return NULL;
-
-		return fork_next;
-	}
+typedef struct count_token {
+	token token;
+	int count;
+	const char* header;
 };
+
+void countup(count_token* self) {
+	if (0 == self->count) {
+		printf("--start:%s--\n", self->header);
+		self->count++;
+		suspend(self);
+	}
+	else if (self->count < 10) {
+		printf("--busy:%s%d--\n", self->header, self->count++);
+		suspend(self);
+	}
+	else
+	{
+		printf("--end:%s--\n", self->header);
+	}
+
+}
 
 //10までカウントアップして終えるジョブ
-class job1 : public job {
-	int count = 0;
-	const char* header;
-public:
+//struct job1 : job {
+//private:
+//	int count = 0;
+//	const char* header;
+//public:
+//
+//	job1(int count, const char* header) {
+//		this->count = count;
+//		this->header = header;
+//	}
+//
+//	virtual void func() override {
+//		if (0 == count)
+//		{
+//			count++;
+//			printf("--start%s--\n", header);
+//			suspend();
+//		}
+//		else if (count < 10) {
+//			printf("busy%s:%d\n", header, count++);
+//			suspend();
+//		}
+//		else {
+//			printf("--end%s--\n", header);
+//		}
+//	}
+//};
 
-	job1(int count, const char* header) {
-		this->count = count;
-		this->header = header;
-	}
+void token_init(token* self, execute func) {
+	self->execute = func;
+	self->action_next = NULL;
+	self->fork_next = self;
+	self->errno = JOB_STATE_START;
+}
 
-	virtual void func() override
-	{
-		if (0 == count)
-		{
-			count++;
-			printf("--start%s--\n", header);
-			suspend();
-		}
-		else if (count < 10) {
-			printf("busy%s:%d\n", header, count++);
-			suspend();
-		}
-		else {
-			printf("--end%s--\n", header);
-		}
-	}
-};
+void countup_init(count_token* self, const char* header, int count) {
+	self->header = header;
+	self->count = count;
+	token_init(&self->token, (execute)countup);
+}
 
 /// <summary>
 /// メイン処理
@@ -149,22 +124,39 @@ public:
 /// <returns></returns>
 int main(void) {
 
-	auto j = new job1(-10, "A");
-	auto l = new job1(0, "C");
-	job* k = new job1(0, "B");
-	auto m = new job1(0, "M");
-	k->fork(m);
-	j->fork(k);
-	j->action(l);
+	count_token t1;
+	count_token t2;
+	count_token t3;
+	count_token t4;
+	countup_init(&t1, "A", -5);
+	countup_init(&t2, "B", 0);
+	countup_init(&t3, "C", 0);
+	countup_init(&t4, "D", 0);
+	fork((token*)&t2, (token*)&t3);
+	fork((token*)&t1, (token*)&t2);
+	//action((token*)&t2, (token*)&t3);
+	//fork((token*)&t3, (token*)&t4);
 
-	//全て完了するまで待つ場合
-	job* x = j;
-	while (NULL != x)
-		x = x->dispatch();
+	token* exe = &t1.token;
+	while (NULL != exe)
+		exe = dispatch(exe);
 
-	//特定のジョブ完了するまで待つ場合
-	//j->wait();
-	l->wait(j);
+	//auto j = new job1(-10, "A");
+	//auto l = new job1(0, "C");
+	//job* k = new job1(0, "B");
+	//auto m = new job1(0, "M");
+	//k->fork(m);
+	//j->fork(k);
+	//j->action(l);
+
+	////全て完了するまで待つ場合
+	//job* x = j;
+	//while (NULL != x)
+	//	x = x->dispatch();
+
+	////特定のジョブ完了するまで待つ場合
+	////j->wait();
+	//l->wait(j);
 
 	return 1;
 }
